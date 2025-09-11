5671d63011780645a8e5b7d79310c1efe1eb216d
MordaK <msroshanbin95@gmail.com>
2025-09-11 19:19:36 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -314,16 +314,20 @@ server {
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Host $host;
+        proxy_set_header X-Forwarded-Port $server_port;
     }
 
-    # Subscription page under /sub
-    location /sub/ {
+    # Subscription page under /api/sub
+    location /api/sub/ {
         proxy_http_version 1.1;
         proxy_pass http://remnawave_subscription/;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Host $host;
+        proxy_set_header X-Forwarded-Port $server_port;
     }
 
     # SSL Configuration (Mozilla Intermediate Guidelines)
